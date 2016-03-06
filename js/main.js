2b0936cf2e201b312d4a9bb9f6b983ba90c89661
turkey321 <1280378263@qq.com>
2016-03-06 19:27:58 +0800

    thrid014

--- js/main.js
+++ js/main.js
@@ -461,11 +461,3 @@ function getMousePosition(event) {
 	mouseY = event.pageY;
 	imageOne.setImageDirection();
 }
-
-//离开页面改变titile的方法
-window.onblur = function() {
-	document.title = '你出现了一个BUG！';
-};
-window.onfocus = function() {
-	document.title = '符海飞:"哈哈,骗你的~"';
-};
