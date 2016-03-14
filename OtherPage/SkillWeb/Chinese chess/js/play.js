d77f5c80008739cb29c4c6bc0dfcb3f6b74bd7be
turkey321 <1280378263@qq.com>
2016-03-14 21:51:59 +0800

    thrid033

--- OtherPage/SkillWeb/Chinese chess/js/play.js
+++ OtherPage/SkillWeb/Chinese chess/js/play.js
@@ -1,69 +1,69 @@
-/*! Ò»Ò¶¹ÂÖÛ | qq:28701884 | »¶Ó­Ö¸½Ì */
+/*! ä¸€å¶å­¤èˆŸ | qq:28701884 | æ¬¢è¿æŒ‡æ•™ */
 
-var play = play||{};
+var play = play || {};
 
-play.init = function (){
-	
-	play.my				=	1;				//Íæ¼Ò·½
-	play.map 			=	com.arr2Clone (com.initMap);		//³õÊ¼»¯ÆåÅÌ
-	play.nowManKey		=	false;			//ÏÖÔÚÒª²Ù×÷µÄÆå×Ó
-	play.pace 			=	[];				//¼ÇÂ¼Ã¿Ò»²½
-	play.isPlay 		=	true ;			//ÊÇ·ñÄÜ×ßÆå
-	play.mans 			=	com.mans;
-	play.bylaw 			= 	com.bylaw;
-	play.show 			= 	com.show;
-	play.showPane 		= 	com.showPane;
-	play.isOffensive	=	true;			//ÊÇ·ñÏÈÊÖ
-	play.depth			=	play.depth || 3;				//ËÑË÷Éî¶È
-	
-	play.isFoul			=	false;	//ÊÇ·ñ·¸¹æ³¤½«
-	
-	
-	
-	com.pane.isShow		=	 false;			//Òş²Ø·½¿é
-	
-	//³õÊ¼»¯Æå×Ó
-	for (var i=0; i<play.map.length; i++){
-		for (var n=0; n<play.map[i].length; n++){
+play.init = function() {
+
+	play.my = 1; //ç©å®¶æ–¹
+	play.map = com.arr2Clone(com.initMap); //åˆå§‹åŒ–æ£‹ç›˜
+	play.nowManKey = false; //ç°åœ¨è¦æ“ä½œçš„æ£‹å­
+	play.pace = []; //è®°å½•æ¯ä¸€æ­¥
+	play.isPlay = true; //æ˜¯å¦èƒ½èµ°æ£‹
+	play.mans = com.mans;
+	play.bylaw = com.bylaw;
+	play.show = com.show;
+	play.showPane = com.showPane;
+	play.isOffensive = true; //æ˜¯å¦å…ˆæ‰‹
+	play.depth = play.depth || 3; //æœç´¢æ·±åº¦
+
+	play.isFoul = false; //æ˜¯å¦çŠ¯è§„é•¿å°†
+
+
+
+	com.pane.isShow = false; //éšè—æ–¹å—
+
+	//åˆå§‹åŒ–æ£‹å­
+	for (var i = 0; i < play.map.length; i++) {
+		for (var n = 0; n < play.map[i].length; n++) {
 			var key = play.map[i][n];
-			if (key){
-				com.mans[key].x=n;
-				com.mans[key].y=i;
+			if (key) {
+				com.mans[key].x = n;
+				com.mans[key].y = i;
 				com.mans[key].isShow = true;
 			}
 		}
 	}
 	play.show();
-	
-	//°ó¶¨µã»÷ÊÂ¼ş
-	com.canvas.addEventListener("click",play.clickCanvas)
-	//clearInterval(play.timer);
-	//com.get("autoPlay").addEventListener("click", function(e) {
+
+	//ç»‘å®šç‚¹å‡»äº‹ä»¶
+	com.canvas.addEventListener("click", play.clickCanvas)
+		//clearInterval(play.timer);
+		//com.get("autoPlay").addEventListener("click", function(e) {
 		//clearInterval(play.timer);
 		//play.timer = setInterval("play.AIPlay()",1000);
-	//	play.AIPlay()
-	//})
-	/*
-	com.get("offensivePlay").addEventListener("click", function(e) {
-		play.isOffensive=true;
-		play.isPlay=true ;	
-		com.get("chessRight").style.display = "none";
-		play.init();
-	})
+		//	play.AIPlay()
+		//})
+		/*
+		com.get("offensivePlay").addEventListener("click", function(e) {
+			play.isOffensive=true;
+			play.isPlay=true ;	
+			com.get("chessRight").style.display = "none";
+			play.init();
+		})
 	
-	com.get("defensivePlay").addEventListener("click", function(e) {
-		play.isOffensive=false;
-		play.isPlay=true ;	
-		com.get("chessRight").style.display = "none";
-		play.init();
-	})
-	*/
-	
-	
+		com.get("defensivePlay").addEventListener("click", function(e) {
+			play.isOffensive=false;
+			play.isPlay=true ;	
+			com.get("chessRight").style.display = "none";
+			play.init();
+		})
+		*/
+
+
 	com.get("regretBn").addEventListener("click", function(e) {
 		play.regret();
 	})
-	
+
 	/*
 	var initTime = new Date().getTime();
 	for (var i=0; i<=100000; i++){
@@ -77,113 +77,113 @@ play.init = function (){
 	var nowTime= new Date().getTime();
 	z([h,nowTime-initTime])
 	*/
-	
+
 }
 
 
 
-//»ÚÆå
-play.regret = function (){
-	var map  = com.arr2Clone(com.initMap);
-	//³õÊ¼»¯ËùÓĞÆå×Ó
-	for (var i=0; i<map.length; i++){
-		for (var n=0; n<map[i].length; n++){
+//æ‚”æ£‹
+play.regret = function() {
+	var map = com.arr2Clone(com.initMap);
+	//åˆå§‹åŒ–æ‰€æœ‰æ£‹å­
+	for (var i = 0; i < map.length; i++) {
+		for (var n = 0; n < map[i].length; n++) {
 			var key = map[i][n];
-			if (key){
-				com.mans[key].x=n;
-				com.mans[key].y=i;
+			if (key) {
+				com.mans[key].x = n;
+				com.mans[key].y = i;
 				com.mans[key].isShow = true;
 			}
 		}
 	}
-	var pace= play.pace;
+	var pace = play.pace;
 	pace.pop();
 	pace.pop();
-	
-	for (var i=0; i<pace.length; i++){
-		var p= pace[i].split("")
+
+	for (var i = 0; i < pace.length; i++) {
+		var p = pace[i].split("")
 		var x = parseInt(p[0], 10);
 		var y = parseInt(p[1], 10);
 		var newX = parseInt(p[2], 10);
 		var newY = parseInt(p[3], 10);
-		var key=map[y][x];
+		var key = map[y][x];
 		//try{
-	 
-		var cMan=map[newY][newX];
+
+		var cMan = map[newY][newX];
 		if (cMan) com.mans[map[newY][newX]].isShow = false;
 		com.mans[key].x = newX;
 		com.mans[key].y = newY;
 		map[newY][newX] = key;
 		delete map[y][x];
-		if (i==pace.length-1){
-			com.showPane(newX ,newY,x,y)	
+		if (i == pace.length - 1) {
+			com.showPane(newX, newY, x, y)
 		}
 		//} catch (e){
 		//	com.show()
 		//	z([key,p,pace,map])
-			
+
 		//	}
 	}
 	play.map = map;
-	play.my=1;
-	play.isPlay=true;
+	play.my = 1;
+	play.isPlay = true;
 	com.show();
 }
 
 
 
-//µã»÷ÆåÅÌÊÂ¼ş
-play.clickCanvas = function (e){
+//ç‚¹å‡»æ£‹ç›˜äº‹ä»¶
+play.clickCanvas = function(e) {
 	if (!play.isPlay) return false;
 	var key = play.getClickMan(e);
 	var point = play.getClickPoint(e);
-	
+
 	var x = point.x;
 	var y = point.y;
-	
-	if (key){
-		play.clickMan(key,x,y);	
-	}else {
-		play.clickPoint(x,y);	
+
+	if (key) {
+		play.clickMan(key, x, y);
+	} else {
+		play.clickPoint(x, y);
 	}
-	play.isFoul = play.checkFoul();//¼ì²âÊÇ²»ÊÇ³¤½«
+	play.isFoul = play.checkFoul(); //æ£€æµ‹æ˜¯ä¸æ˜¯é•¿å°†
 }
 
-//µã»÷Æå×Ó£¬Á½ÖÖÇé¿ö£¬Ñ¡ÖĞ»òÕß³Ô×Ó
-play.clickMan = function (key,x,y){
+//ç‚¹å‡»æ£‹å­ï¼Œä¸¤ç§æƒ…å†µï¼Œé€‰ä¸­æˆ–è€…åƒå­
+play.clickMan = function(key, x, y) {
 	var man = com.mans[key];
-	//³Ô×Ó
-	if (play.nowManKey&&play.nowManKey != key && man.my != com.mans[play.nowManKey ].my){
-		//manÎª±»³ÔµôµÄÆå×Ó
-		if (play.indexOfPs(com.mans[play.nowManKey].ps,[x,y])){
+	//åƒå­
+	if (play.nowManKey && play.nowManKey != key && man.my != com.mans[play.nowManKey].my) {
+		//manä¸ºè¢«åƒæ‰çš„æ£‹å­
+		if (play.indexOfPs(com.mans[play.nowManKey].ps, [x, y])) {
 			man.isShow = false;
-			var pace=com.mans[play.nowManKey].x+""+com.mans[play.nowManKey].y
-			//z(bill.createMove(play.map,man.x,man.y,x,y))
+			var pace = com.mans[play.nowManKey].x + "" + com.mans[play.nowManKey].y
+				//z(bill.createMove(play.map,man.x,man.y,x,y))
 			delete play.map[com.mans[play.nowManKey].y][com.mans[play.nowManKey].x];
 			play.map[y][x] = play.nowManKey;
-			com.showPane(com.mans[play.nowManKey].x ,com.mans[play.nowManKey].y,x,y)
+			com.showPane(com.mans[play.nowManKey].x, com.mans[play.nowManKey].y, x, y)
 			com.mans[play.nowManKey].x = x;
 			com.mans[play.nowManKey].y = y;
 			com.mans[play.nowManKey].alpha = 1
-			
-			play.pace.push(pace+x+y);
+
+			play.pace.push(pace + x + y);
 			play.nowManKey = false;
 			com.pane.isShow = false;
 			com.dot.dots = [];
 			com.show()
 			com.get("clickAudio").play();
-			setTimeout("play.AIPlay()",500);
-			if (key == "j0") play.showWin (-1);
-			if (key == "J0") play.showWin (1);
+			setTimeout("play.AIPlay()", 500);
+			if (key == "j0") play.showWin(-1);
+			if (key == "J0") play.showWin(1);
 		}
-	// Ñ¡ÖĞÆå×Ó
-	}else{
-		if (man.my===1){
-			if (com.mans[play.nowManKey]) com.mans[play.nowManKey].alpha = 1 ;
+		// é€‰ä¸­æ£‹å­
+	} else {
+		if (man.my === 1) {
+			if (com.mans[play.nowManKey]) com.mans[play.nowManKey].alpha = 1;
 			man.alpha = 0.6;
 			com.pane.isShow = false;
 			play.nowManKey = key;
-			com.mans[key].ps = com.mans[key].bl(); //»ñµÃËùÓĞÄÜ×Åµã
+			com.mans[key].ps = com.mans[key].bl(); //è·å¾—æ‰€æœ‰èƒ½ç€ç‚¹
 			com.dot.dots = com.mans[key].ps
 			com.show();
 			//com.get("selectAudio").start(0);
@@ -192,136 +192,138 @@ play.clickMan = function (key,x,y){
 	}
 }
 
-//µã»÷×Åµã
-play.clickPoint = function (x,y){
-	var key=play.nowManKey;
-	var man=com.mans[key];
-	if (play.nowManKey){
-		if (play.indexOfPs(com.mans[key].ps,[x,y])){
-			var pace=man.x+""+man.y
-			//z(bill.createMove(play.map,man.x,man.y,x,y))
+//ç‚¹å‡»ç€ç‚¹
+play.clickPoint = function(x, y) {
+	var key = play.nowManKey;
+	var man = com.mans[key];
+	if (play.nowManKey) {
+		if (play.indexOfPs(com.mans[key].ps, [x, y])) {
+			var pace = man.x + "" + man.y
+				//z(bill.createMove(play.map,man.x,man.y,x,y))
 			delete play.map[man.y][man.x];
 			play.map[y][x] = key;
-			com.showPane(man.x ,man.y,x,y)
+			com.showPane(man.x, man.y, x, y)
 			man.x = x;
 			man.y = y;
 			man.alpha = 1;
-			play.pace.push(pace+x+y);
+			play.pace.push(pace + x + y);
 			play.nowManKey = false;
 			com.dot.dots = [];
 			com.show();
 			com.get("clickAudio").play();
-			setTimeout("play.AIPlay()",500);
-		}else{
-			//alert("²»ÄÜÕâÃ´×ßÅ¶£¡")	
+			setTimeout("play.AIPlay()", 500);
+		} else {
+			//alert("ä¸èƒ½è¿™ä¹ˆèµ°å“¦ï¼")	
 		}
 	}
-	
+
 }
 
-//Ai×Ô¶¯×ßÆå
-play.AIPlay = function (){
+//Aiè‡ªåŠ¨èµ°æ£‹
+play.AIPlay = function() {
 	//return
-	play.my = -1 ;
-	var pace=AI.init(play.pace.join(""))
+	play.my = -1;
+	var pace = AI.init(play.pace.join(""))
 	if (!pace) {
-		play.showWin (1);
-		return ;
+		play.showWin(1);
+		return;
 	}
 	play.pace.push(pace.join(""));
-	var key=play.map[pace[1]][pace[0]]
-		play.nowManKey = key;
-	
-	var key=play.map[pace[3]][pace[2]];
-	if (key){
-		play.AIclickMan(key,pace[2],pace[3]);	
-	}else {
-		play.AIclickPoint(pace[2],pace[3]);	
+	var key = play.map[pace[1]][pace[0]]
+	play.nowManKey = key;
+
+	var key = play.map[pace[3]][pace[2]];
+	if (key) {
+		play.AIclickMan(key, pace[2], pace[3]);
+	} else {
+		play.AIclickPoint(pace[2], pace[3]);
 	}
 	com.get("clickAudio").play();
-	
-	
+
+
 }
 
-//¼ì²éÊÇ·ñ³¤½«
-play.checkFoul = function(){
-	var p=play.pace;
-	var len=parseInt(p.length,10);
-	if (len>11&&p[len-1] == p[len-5] &&p[len-5] == p[len-9]){
-		return p[len-4].split("");
+//æ£€æŸ¥æ˜¯å¦é•¿å°†
+play.checkFoul = function() {
+	var p = play.pace;
+	var len = parseInt(p.length, 10);
+	if (len > 11 && p[len - 1] == p[len - 5] && p[len - 5] == p[len - 9]) {
+		return p[len - 4].split("");
 	}
 	return false;
 }
 
 
 
-play.AIclickMan = function (key,x,y){
+play.AIclickMan = function(key, x, y) {
 	var man = com.mans[key];
-	//³Ô×Ó
+	//åƒå­
 	man.isShow = false;
 	delete play.map[com.mans[play.nowManKey].y][com.mans[play.nowManKey].x];
 	play.map[y][x] = play.nowManKey;
-	play.showPane(com.mans[play.nowManKey].x ,com.mans[play.nowManKey].y,x,y)
-	
+	play.showPane(com.mans[play.nowManKey].x, com.mans[play.nowManKey].y, x, y)
+
 	com.mans[play.nowManKey].x = x;
 	com.mans[play.nowManKey].y = y;
 	play.nowManKey = false;
-	
+
 	com.show()
-	if (key == "j0") play.showWin (-1);
-	if (key == "J0") play.showWin (1);
+	if (key == "j0") play.showWin(-1);
+	if (key == "J0") play.showWin(1);
 }
 
-play.AIclickPoint = function (x,y){
-	var key=play.nowManKey;
-	var man=com.mans[key];
-	if (play.nowManKey){
+play.AIclickPoint = function(x, y) {
+	var key = play.nowManKey;
+	var man = com.mans[key];
+	if (play.nowManKey) {
 		delete play.map[com.mans[play.nowManKey].y][com.mans[play.nowManKey].x];
 		play.map[y][x] = key;
-		
-		com.showPane(man.x,man.y,x,y)
-		
-	
+
+		com.showPane(man.x, man.y, x, y)
+
+
 		man.x = x;
 		man.y = y;
 		play.nowManKey = false;
-		
+
 	}
 	com.show();
 }
 
 
-play.indexOfPs = function (ps,xy){
-	for (var i=0; i<ps.length; i++){
-		if (ps[i][0]==xy[0]&&ps[i][1]==xy[1]) return true;
+play.indexOfPs = function(ps, xy) {
+	for (var i = 0; i < ps.length; i++) {
+		if (ps[i][0] == xy[0] && ps[i][1] == xy[1]) return true;
 	}
 	return false;
-	
+
 }
 
-//»ñµÃµã»÷µÄ×Åµã
-play.getClickPoint = function (e){
+//è·å¾—ç‚¹å‡»çš„ç€ç‚¹
+play.getClickPoint = function(e) {
 	var domXY = com.getDomXY(com.canvas);
-	var x=Math.round((e.pageX-domXY.x-com.pointStartX-20)/com.spaceX)
-	var y=Math.round((e.pageY-domXY.y-com.pointStartY-20)/com.spaceY)
-	return {"x":x,"y":y}
-}
-
-//»ñµÃÆå×Ó
-play.getClickMan = function (e){
-	var clickXY=play.getClickPoint(e);
-	var x=clickXY.x;
-	var y=clickXY.y;
-	if (x < 0 || x>8 || y < 0 || y > 9) return false;
-	return (play.map[y][x] && play.map[y][x]!="0") ? play.map[y][x] : false;
-}
-
-play.showWin = function (my){
-	play.isPlay = false;
-	if (my===1){
-		alert("¹§Ï²Äã£¬ÄãÓ®ÁË£¡");
-	}else{
-		alert("ºÜÒÅº¶£¬ÄãÊäÁË£¡");
+	var x = Math.round((e.pageX - domXY.x - com.pointStartX - 20) / com.spaceX)
+	var y = Math.round((e.pageY - domXY.y - com.pointStartY - 20) / com.spaceY)
+	return {
+		"x": x,
+		"y": y
 	}
 }
 
+//è·å¾—æ£‹å­
+play.getClickMan = function(e) {
+	var clickXY = play.getClickPoint(e);
+	var x = clickXY.x;
+	var y = clickXY.y;
+	if (x < 0 || x > 8 || y < 0 || y > 9) return false;
+	return (play.map[y][x] && play.map[y][x] != "0") ? play.map[y][x] : false;
+}
+
+play.showWin = function(my) {
+	play.isPlay = false;
+	if (my === 1) {
+		alert("æ­å–œä½ ï¼Œä½ èµ¢äº†ï¼");
+	} else {
+		alert("å¾ˆé—æ†¾ï¼Œä½ è¾“äº†ï¼");
+	}
+}
